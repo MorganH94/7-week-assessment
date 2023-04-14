b7eb1f53d6474b5b1f854d018801d13345ae6218
Morgan Herrmann <morganherrmann4@gmail.com>
2023-04-14 11:49:56 -0700

    completed assessment

---
+++ cs/cs/new-code.js
@@ -0,0 +1,61 @@
+let array = [1, 4, 11, 2, 37, -4]
+
+let value = false
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) {
+      if (array[i] + array[j] === 0) {
+        value = true
+      } 
+    }
+  }
+}
+console.log(value)
+runtime: O(n^2)
+space complexity: O(1)
+
+
+
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    return uniqueChars.size === word.length
+  }
+runtime: O(n)
+space complexity: O(n)
+
+
+
+const string = 'The quick brown fox jumps over the lazy dog!';
+function isPangram(string){
+    let strArr = string.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    for (let i = 0; i < alphabet.length; i++) {
+      if(strArr.indexOf(alphabet[i]) < 0){
+        return false;
+      }
+    }
+    return true;
+  }
+runtime: O(n)
+space complexity: O(n)
+
+
+
+function findLongestWord(str) {
+    let strSplit = str.split(' ');
+    let longestWord = 0;
+    for(let i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > longestWord){
+      longestWord = strSplit[i].length;
+       }
+    }
+    return longestWord;
+  }
+  findLongestWord("hi", "hello");
+runtime: O(n)
+space complexity: O(n)
